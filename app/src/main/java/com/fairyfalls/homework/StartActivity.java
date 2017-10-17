ca5ec284b19cb0c0fa3e145f8ec1cd524f409f5b
Andrey543 <andreyrybakovoutlook@gmail.com>
2017-10-17 21:08:29 +0300

    New icon

--- app/src/main/java/com/fairyfalls/homework/StartActivity.java
+++ app/src/main/java/com/fairyfalls/homework/StartActivity.java
@@ -12,27 +12,25 @@ import android.widget.CheckBox;
 
 public class StartActivity extends AppCompatActivity {
 
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
+    }
+    protected void onResume(){
+        super.onResume();
 
         new CountDownTimer(2000, 1){
             public void onTick(long millisUntilFinished) {
             }
 
             public void onFinish() {
-                Intent intent = new Intent(StartActivity.this, CounterActivity.class);
-                startActivity(intent);
+
+                    Intent intent = new Intent(StartActivity.this, CounterActivity.class);
+                    startActivity(intent);
             }
 
         }.start();
-
     }
 
 }
